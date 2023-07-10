5a7ed45294760c9f43dc436fd5351f0d2f0c656e
Jose Perez <jmperezvzla@gmail.com>
2023-07-10 02:33:47 -0400

    categories & products search

--- nordichomes/core/views.py
+++ nordichomes/core/views.py
@@ -1,6 +1,7 @@
+from django.db.models import Q
 from django.shortcuts import render
 
-from product.models import Product
+from product.models import Product, Category
 
 # Create your views here.
 def frontpage(request):
@@ -9,8 +10,24 @@ def frontpage(request):
         'products': products
     })
 
+
 def shop(request):
+    categories = Category.objects.all()
     products = Product.objects.all()
-    return render(request, 'core/shop.html', {
-        'products': products
-    })
+
+    active_category = request.GET.get('category', '')
+    query = request.GET.get('query', '')
+
+    if active_category:
+        products = products.filter(category__slug=active_category)
+
+    if query:
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    context = {
+        'categories': categories,
+        'products': products,
+        'active_category': active_category,
+    }
+
+    return render(request, 'core/shop.html', context)
