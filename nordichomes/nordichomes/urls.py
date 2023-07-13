7e344d400225eb32859dd270539c7a0d1a66c652
Jose Perez <jmperezvzla@gmail.com>
2023-07-12 23:51:16 -0400

    Create cart & checkout template html

--- nordichomes/nordichomes/urls.py
+++ nordichomes/nordichomes/urls.py
@@ -22,7 +22,7 @@ from django.urls import path
 
 from core.views import frontpage, shop, signup, login_old
 from product.views import product
-from cart.views import add_to_cart
+from cart.views import add_to_cart, cart, checkout
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
@@ -31,6 +31,8 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'),
+    path('cart/', cart, name='cart'),
+    path('cart/checkout/', checkout, name='checkout'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
